daa00f0ed9f9761c0b47c5a26f4ab25bd36a5e7b
lsy093 <836659259@qq.com>
2018-12-20 17:43:46 +0800

    添加注释

--- src/main/java/Web/Application.java
+++ src/main/java/Web/Application.java
@@ -3,7 +3,7 @@ package Web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//需要添加@SpringBootApplication注解
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
