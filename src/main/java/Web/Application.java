7b94f432fb5e1174ad9034ac7c499eefd93b9ec3
lsy093 <836659259@qq.com>
2018-12-25 17:20:50 +0800

    springboot JDBC

--- src/main/java/Web/Application.java
+++ src/main/java/Web/Application.java
@@ -1,13 +1,21 @@
 package Web;
 
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 //需要添加@SpringBootApplication注解
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
+		List<Map<String, Object>> result = jdbcTemplate.queryForList("SELECT * FROM ROOM");
+		System.out.println(result);
 	}
 
 }
