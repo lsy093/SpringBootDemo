c3a298c883949f4bb93bd2ed900d865412b6be8f
Li <shengyuan.a.li@accenture.com>
2020-07-30 17:28:18 +0800

    重新开始SpringBoot搭建

--- src/main/java/com/Controller/Application.java
+++ src/main/java/com/Controller/Application.java
@@ -1,18 +1,11 @@
 package com.Controller;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-//需要添加@SpringBootApplication注解
 @SpringBootApplication
-@ComponentScan(basePackages="com.*")
 public class Application {
 	public static void main(String[] args) {
-		
 		SpringApplication.run(Application.class, args);
-		
 	}
-
 }
